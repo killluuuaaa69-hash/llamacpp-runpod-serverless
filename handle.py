ed87396bc176d2efd060f7efd344fc9dbf6c57b6
Kilua <killluuuaaa69@gmail.com>
2025-12-19 05:15:36 +0530

    Update handle.py
    added logits_all : True for looking into log probs

--- handle.py
+++ handle.py
@@ -10,6 +10,7 @@ args = json.loads(os.environ.get("LLAMA_ARGS", "{}"))
 args.setdefault("n_ctx", 32768)
 args.setdefault("n_gpu_layers", -1)
 args.setdefault("flash_attn", True)
+args.setdefault("logits_all",True)
 llm = Llama(**args)
 MODEL_DIR = "/models"
 
